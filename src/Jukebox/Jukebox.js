c3cab445d75ad530c97811bd1816f2938131aa7e
Adrian Berger <adigladi@n172-p241.eduroam.kth.se>
2018-04-12 14:31:38 +0200

    Trying to fix

--- src/Jukebox/Jukebox.js
+++ src/Jukebox/Jukebox.js
@@ -1,32 +1,30 @@
 import React, { Component } from 'react';
 import './Jukebox.css';
-import DZ from 'http://cdn-files.deezer.com/js/min/dz.js';
 
 class Jukebox extends Component {
 
   constructor(props) {
     super(props)
-
+    DZ.init({
+      appId  : '8',
+      channelUrl : 'http://developers.deezer.com/examples/channel.php',
+      player : {
+        
+      }
+      });
     this.state = {
       //numberOfGuests: this.props.model.getNumberOfGuests()
     }
   }
 
   componentDidMount() {
-    DZ.init({
-      appId  : '8',
-      channelUrl : 'http://developers.deezer.com/examples/channel.php',
-      player : {
-        onload : onPlayerLoaded
-      }
-    });
+    console.log("hej");
+    
   
-  function onPlayerLoaded() {
-    DZ.player.playAlbum(238939);
-  }
 }
 
   render() {
+    
     return (
       <div className="Jukebox">
         <h2>This is the Jukebox screen</h2>
