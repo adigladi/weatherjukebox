72ceecbe58cf0200be886bef2f0cc122fb021afc
Adrian Berger <adigladi@n172-p241.eduroam.kth.se>
2018-04-12 12:35:49 +0200

    Jukebox function updated

--- src/Jukebox/Jukebox.js
+++ src/Jukebox/Jukebox.js
@@ -2,10 +2,35 @@ import React, { Component } from 'react';
 import './Jukebox.css';
 
 class Jukebox extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      //numberOfGuests: this.props.model.getNumberOfGuests()
+    }
+  }
+
+  componentDidMount() {
+    DZ.init({
+      appId  : '8',
+      channelUrl : 'http://developers.deezer.com/examples/channel.php',
+      player : {
+        onload : onPlayerLoaded
+      }
+    });
+  }
+
   render() {
     return (
       <div className="Jukebox">
         <h2>This is the Jukebox screen</h2>
+        <div id="dz-root"></div>
+        <div id="controlers">
+          <input type="button" onclick="DZ.player.playAlbum(238939); return false;" value="Play Daft Punk - Discovery"/>
+          <input type="button" onclick="DZ.player.play(); return false;" value="play"/>
+          <input type="button" onclick="DZ.player.pause(); return false;" value="pause"/>
+        </div>
       </div>
     );
   }
