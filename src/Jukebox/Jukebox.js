085f7ca51c6465bcdc4f5479b12bbf9b40b545ba
Martin Wedberg <MartinWedberg@n174-p165.eduroam.kth.se>
2018-04-13 11:10:38 +0200

    dsf

--- src/Jukebox/Jukebox.js
+++ src/Jukebox/Jukebox.js
@@ -32,8 +32,8 @@ class Jukebox extends Component {
   render() {
     return (
       <div className="Jukebox">
-        <h2>Weather:{this.state.weather.description}</h2>
-        <h2>Genre:{this.state.genre.name}</h2>
+        <h2>Weather: {this.state.weather.description}</h2>
+        <h2>Genre: {this.state.genre.name}</h2>
         <iframe scrolling="no" frameBorder="0" src="https://www.deezer.com/plugins/player?format=square&autoplay=true&playlist=true&width=300&height=300&color=007FEB&emptyPlayer=true&layout=dark&size=medium&type=tracks&id=3097520&app_id=1" width="0" height="0"></iframe>
       </div>
     );
