76bc4dc62378fd6f0274a5eba9477651e3d7182e
Emil Dickson <edickson@kth.se>
2018-04-13 10:35:00 +0200

    Switched ID

--- src/Jukebox/Jukebox.js
+++ src/Jukebox/Jukebox.js
@@ -7,7 +7,7 @@ class Jukebox extends Component {
     return (
       <div className="Jukebox">
         <h2>This is the Jukebox screen</h2>
-        <iframe scrolling="no" frameBorder="0" src="https://www.deezer.com/plugins/player?format=square&autoplay=true&playlist=true&width=300&height=300&color=007FEB&emptyPlayer=true&layout=dark&size=medium&type=tracks&id=1560273&app_id=1" width="0" height="0"></iframe>
+        <iframe scrolling="no" frameBorder="0" src="https://www.deezer.com/plugins/player?format=square&autoplay=true&playlist=true&width=300&height=300&color=007FEB&emptyPlayer=true&layout=dark&size=medium&type=tracks&id=3097520&app_id=1" width="0" height="0"></iframe>
       </div>
     );
   }
