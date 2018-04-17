b2c30a747432ce9223441adf20600b2e55501ff7
Martin Wedberg <MartinWedberg@n158-p225.eduroam.kth.se>
2018-04-17 13:43:07 +0200

    modellen fungerar i user

--- src/User/User.js
+++ src/User/User.js
@@ -1,30 +1,23 @@
 import React, { Component } from 'react';
 import './User.css';
+import { modelInstance } from '../data/WeatherModel.js'
 class User extends Component {
 
   constructor(props) {
     super(props)
     
-    // we put on state the properties we want to use and modify in the component
     this.state = {
     }
   }
 
-  // this methods is called by React lifecycle when the 
-  // component is actually shown to the user (mounted to DOM)
-  // that's a good place to setup model observer
   componentDidMount() {
- //   modelInstance.addObserver(this)
+    modelInstance.addObserver(this)
   }
 
-  // this is called when component is removed from the DOM
-  // good place to remove observer
   componentWillUnmount() {
-//    modelInstance.removeObserver(this)
+    modelInstance.removeObserver(this)
   }
 
-  // in our update function we modify the state which will
-  // cause the component to re-render
   update() {
     this.setState({
     })
