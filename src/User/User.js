a56f29fdc621a352fbc93677a81a6df3d2ea807f
Martin Wedberg <MartinWedberg@n158-p225.eduroam.kth.se>
2018-04-17 13:35:21 +0200

    nytt

--- src/User/User.js
+++ src/User/User.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './User.css';
-import { modelInstance } from './data/WeatherModel'
 class User extends Component {
 
   constructor(props) {
@@ -15,13 +14,13 @@ class User extends Component {
   // component is actually shown to the user (mounted to DOM)
   // that's a good place to setup model observer
   componentDidMount() {
-    modelInstance.addObserver(this)
+ //   modelInstance.addObserver(this)
   }
 
   // this is called when component is removed from the DOM
   // good place to remove observer
   componentWillUnmount() {
-    modelInstance.removeObserver(this)
+//    modelInstance.removeObserver(this)
   }
 
   // in our update function we modify the state which will
