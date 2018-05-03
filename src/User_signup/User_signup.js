0dbbb923e98d9f1928db6ee92a3dd664aaebf854
Emil Dickson <edickson@kth.se>
2018-05-03 13:31:40 +0200

    signup and such. under construction.

--- src/User_signup/User_signup.js
+++ src/User_signup/User_signup.js
@@ -2,14 +2,29 @@ import React, { Component } from 'react';
 import './User_signup.css';
 import { Link } from 'react-router-dom';
 import { modelInstance } from '../data/WeatherModel.js'
+
+const INITIAL_STATE = {
+    username: '',
+    email: '',
+    passwordOne: '',
+    passwordTwo: '',
+    error: null,
+};
+
+const byPropKey = (propertyName, value) => () => ({
+    [propertyName]: value,
+});
+
 class User_signup extends Component {
 
     constructor(props) {
         super(props)
 
-        this.state = {
+        this.state = {INITIAL_STATE};
+    }
 
-        }
+    onSubmit = (event) => {
+
     }
 
     componentDidMount() {
@@ -26,14 +41,19 @@ class User_signup extends Component {
 
     render() {
         return (
-            <form id="signupForm">
+            <form id="signupForm" onSubmit={this.onSubmit}>
                 <div className="imgcontainer">
                     <img src={require("./Logo.png")} className="avatar" />
                 </div>
 
                 <div className="container">
                     <label><b>Email:</b></label>
-                    <input type="text" placeholder="Enter email" name="uname" required />
+                    <input 
+                        value={email}
+                        onChange={event => this.setState(byPropKey('email', event.target.value))}
+                        type="text"
+                        placeholder="Enter email"
+                        name="uname" required />
 
                     <label><b>Choose password:</b></label>
                     <input type="password" placeholder="Enter Password" name="psw" required />
@@ -54,7 +74,7 @@ class User_signup extends Component {
                     </Link>
                 </div>
             </form>
-        )
+        );
     }
 
 }
