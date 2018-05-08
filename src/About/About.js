53562a182796b4cd4617c00c4e577eada2c4c904
Martin Wedberg <MartinWedberg@n142-p98.eduroam.kth.se>
2018-05-08 11:23:42 +0200

    about fixes

--- src/About/About.js
+++ src/About/About.js
@@ -35,12 +35,14 @@ class About extends Component {
         </div>
         </div>
         <div className="row container-fluid">
-        <div className="col-12 text-center">
+        <div className="col-1 text-center"/>
+        <div className="col-10 text-center">
             <p id="abouttext">This application let's you enter a location (or fetch one by gps) and then plays you a song based on the local weather conditions. You have the possibility to like the track, thus adding it to you private library, or you could dislike it preventing it from reappearing in further listening.</p>
             <p id="abouttext">In you personal library you can play your liked tracks and find information about the location and weather of the day you liked it!</p>
             <p id="abouttext">On the manage track page you can access your favourite artists deezer pages. Here you're also able to remove tracks from your liked/disliked lists.</p>
             <p id="abouttext">This webpage is responsively designed and well-functioning on mobile phones!</p>
           </div>
+        <div className="col-1 text-center"/>
         </div>
         </div>
     );
